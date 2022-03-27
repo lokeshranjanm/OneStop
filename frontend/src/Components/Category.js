30255ff1c6face6b1fcb323a6908163cd830e745
Lokesh Ranjan <lokeshranjan2021@outlook.com>
2022-03-27 18:15:59 +0530

    new changes

--- frontend/src/Components/Category.js
+++ frontend/src/Components/Category.js
@@ -4,73 +4,59 @@ import Grid from "@mui/material/Grid";
 import { Typography } from "@mui/material";
 import "./Category.css";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+
+const categories = [
+  {
+    image:
+      "https://www.wrl.org/wp-content/uploads/2021/12/various-sports-accessories-pic.jpg",
+    name: "Sports",
+    link: "/sports",
+  },
+  {
+    image:
+      "https://cdn.dribbble.com/users/2130467/screenshots/14393984/media/2c73ab99a7c180d320e14a0cec561509.png?compress=1&resize=400x300&vertical=top",
+    name: "Electroincs",
+    link: "/electronics",
+  },
+  {
+    image:
+      "https://media.istockphoto.com/photos/fashion-clothes-on-a-rack-in-a-light-background-indoors-place-for-picture-id1257563298?k=20&m=1257563298&s=170667a&w=0&h=1ZWgeb1wAK2STpqq8k76dpRSD45NEcd6orUYhvIJ-MA=",
+    name: "Clothing",
+    link: "/clothing",
+  },
+];
+
 function Category() {
   return (
-    <div className="category">
-      <h3 style={{ padding: 20 }}>#Shop Top Categories</h3>
+    <div className="category" style={{ padding: "0px 20px" }}>
+      <h3 style={{ padding: "20px 0px 10px 0px" }}>#Shop Top Categories</h3>
       <Grid container>
-        <Grid item xs={12} md={4} sm={12} className="root">
-          <Link>
-            <a>
-              <div
-                style={{ width: "100%", height: "100%", overflow: "hidden" }}
-              >
-                <div className="backdrop" />
-                <img
-                  src="https://storage.googleapis.com/shoopy-test/2/cat/1629224941565_2_cat.jpg"
-                  className="thumbnail"
-                  alt=""
-                />
-                <div className="nameContainer">
-                  <Typography className="name">Sports </Typography>{" "}
-                </div>
-                <ChevronRightIcon className="rightArrow" />
-              </div>
-            </a>
-          </Link>
-        </Grid>
-        <Grid item xs={12} md={4} sm={12} className="root">
-          <Link>
-            <a>
-              <div
-                style={{ width: "100%", height: "100%", overflow: "hidden" }}
-              >
-                <div className="backdrop">
-                  <img
-                    src="https://res.cloudinary.com/drzudfgzl/image/upload/v1596103536/tvfhvp7qobnl7fqcrwh5.jpg"
-                    className="thumbnail"
-                    alt=""
-                  />
-                  <div className="nameContainer">
-                    <Typography className="name">Electronics </Typography>{" "}
+        {categories.map((category, index) => {
+          return (
+            <Grid key={index} item xs={12} md={4} sm={12} className="root">
+              <Link to={category.link}>
+                <a>
+                  <div
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      overflow: "hidden",
+                    }}
+                  >
+                    <div className="backdrop" />
+                    <img src={category.image} className="thumbnail" alt="" />
+                    <div className="nameContainer">
+                      <Typography className="name">
+                        {category.name}
+                        <ChevronRightIcon className="rightArrow" />
+                      </Typography>
+                    </div>
                   </div>
-                  <ChevronRightIcon className="rightArrow" />
-                </div>
-              </div>
-            </a>
-          </Link>
-        </Grid>
-        <Grid item xs={12} md={4} sm={12} className="root">
-          <Link>
-            <a>
-              <div
-                style={{ width: "100%", height: "100%", overflow: "hidden" }}
-              >
-                <div className="backdrop">
-                  <img
-                    src="https://res.cloudinary.com/drzudfgzl/image/upload/v1596103536/tvfhvp7qobnl7fqcrwh5.jpg"
-                    className="thumbnail"
-                    alt=""
-                  />
-                  <div className="nameContainer">
-                    <Typography className="name">Clothing </Typography>
-                  </div>
-                  <ChevronRightIcon className="rightArrow" />
-                </div>
-              </div>
-            </a>
-          </Link>
-        </Grid>
+                </a>
+              </Link>
+            </Grid>
+          );
+        })}
       </Grid>
     </div>
   );
