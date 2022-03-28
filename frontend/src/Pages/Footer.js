bcba6b1dd7aa0a084cb96f586a245571de8e2b9f
Lokesh Ranjan <lokeshranjan2021@outlook.com>
2022-03-28 18:57:06 +0530

    new changes

--- frontend/src/Pages/Footer.js
+++ frontend/src/Pages/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div style={{ height: "70px", background: "#F50057", marginTop: 20 ,justifyContent: "center", alignItems: "center", display: "flex", fontWeight:"600", color:"white"}}>
-       © 2022 Copyright:OneStop
+       © 2022 Copyright: OneStop
        {/* <a class="text-white" href="https://OneStop.com/">MDBootstrap.com</a> */}
     </div>
   );
